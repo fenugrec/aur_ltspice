96afdf800a60ed19dcab2ba1ade2dfa3e0782d32
fenugrec <fenugrec@users.sourceforge.net>
2025-01-02 12:21:31 -0500

    ltspice.sh wrapper : create initial LTspice.ini
    disable telemetry by default. This can be re-enabled from the GUI if desired

--- ltspice.sh
+++ ltspice.sh
@@ -4,9 +4,13 @@ CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/ltspice"
 if [ ! -d "$HOME"/.ltspice ] ; then
    mkdir -p "$HOME"/.ltspice
 fi
+
 if [ ! -d $CONFIG_DIR ]; then
    mkdir -p $CONFIG_DIR
-   touch $CONFIG_DIR/LTspice.ini
+fi
+if [ ! -f "$CONFIG_DIR/LTspice.ini" ]; then
+   # disable sending telemetry
+   echo -e "[Options]\nCaptureAnalytics=false" > "$CONFIG_DIR/LTspice.ini"
 fi
 
 WINEPREFIX="$HOME"/.local/share/wineprefixes/ltspice
