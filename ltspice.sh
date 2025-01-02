c6ef9d287c1dcfafb92d93b8ba92c7d59b2a06d1
fenugrec <fenugrec@users.sourceforge.net>
2025-01-02 12:16:27 -0500

    create wineprefixes dir if necessary

--- ltspice.sh
+++ ltspice.sh
@@ -8,5 +8,11 @@ if [ ! -d $CONFIG_DIR ]; then
    mkdir -p $CONFIG_DIR
    touch $CONFIG_DIR/LTspice.ini
 fi
-export WINEPREFIX=$HOME/.local/share/wineprefixes/ltspice WINEARCH=win64
+
+WINEPREFIX="$HOME"/.local/share/wineprefixes/ltspice
+if [ ! -d $WINEPREFIX ]; then
+   mkdir -p $WINEPREFIX
+fi
+export WINEARCH=win64
+export WINEPREFIX
 wine /usr/lib/wine/x86_64-windows/start.exe /unix /opt/ltspice/LTspice.exe -ini $CONFIG_DIR/LTspice.ini "$@"
